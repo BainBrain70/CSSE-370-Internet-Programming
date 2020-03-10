9ad9a6a4d956e4e39c4777930aee97ad91760aff
BainBrain70 <33583461+BainBrain70@users.noreply.github.com>
2020-03-10 11:59:12 -0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,5 @@
-var button = createElement("button);
+var button = createElement("button");
 button.innerHTML = "Click Me";
 
 var body = document.getElementsByTagName("body");
-[0];
 body.appendChild(button);
