33a1314ffeceea4bcdfc066bd800c7b855552d9a
BainBrain70 <33583461+BainBrain70@users.noreply.github.com>
2020-05-06 22:13:34 -0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,18 @@
-var button = createElement("button");
-button.innerHTML = "Click Me";
+window.onload=init;
 
-var body = document.getElementsByTagName("body");
-body.appendChild(button);
+async function init(){
+	const url='https://api.openweathermap.org/data/2.5/weather?q=Fresno,us&APPID=87e3c3bb474cc0be0e7747a2d9d2f1fb&units=imperial';
+        const response = await fetch(url);
+        const data = await response.json();
+	document.getElementById("MyData").innerHTML = "The Current Temp is: " + data.main.temp + "F, Click for today's low";
+}
+
+async function getAPI(){
+    const api='https://api.openweathermap.org/data/2.5/weather?q=Fresno,us&APPID=87e3c3bb474cc0be0e7747a2d9d2f1fb&units=imperial';
+    const response = await fetch(api);
+    const data = await response.json();
+    document.getElementById("DailyLow").innerHTML = data.main.temp_min + "F";
+}
+function Click(){
+	document.getElementById('Vacation').style.visibility = "visible";
+}
